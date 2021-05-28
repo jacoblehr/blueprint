16b4883d2ae1b08fd988c432afd38fa1c1e36cb5
jake78 <jake@hutly.com>
2021-05-28 22:22:37 +1000

    Update type returns on entities

--- src/main/db/entities/entity.ts
+++ src/main/db/entities/entity.ts
@@ -43,13 +43,13 @@ export type BulkDeleteOperation<T> = {
 export type Entity<T> = {
 	init: (db: sqlite.Database) => Promise<void>;
 
-	create?: (args: { db: sqlite.Database } & CreateOperation<T>) => Promise<Entity<T>>;
-	read?: (args: { db: sqlite.Database } & ReadOperation<T>) => Promise<Entity<T>>;
-	update?: (args: { db: sqlite.Database } & UpdateOperation<T>) => Promise<Entity<T>>;
-	delete?: (args: { db: sqlite.Database } & DeleteOperation<T>) => Promise<Entity<T>>;
+	create?: (args: { db: sqlite.Database } & CreateOperation<T>) => Promise<T>;
+	read?: (args: { db: sqlite.Database } & ReadOperation<T>) => Promise<T>;
+	update?: (args: { db: sqlite.Database } & UpdateOperation<T>) => Promise<T>;
+	delete?: (args: { db: sqlite.Database } & DeleteOperation<T>) => Promise<T>;
 
-	createBulk?: (args: { db: sqlite.Database } & BulkCreateOperation<T>) => Promise<Array<Entity<T>>>;
-	readBulk?: (args: { db: sqlite.Database } & BulkReadOperation<T>) => Promise<Array<Entity<T>>>;
-	updateBulk?: (args: { db: sqlite.Database } & BulkUpdateOperation<T>) => Promise<Array<Entity<T>>>;
-	deleteBulk?: (args: { db: sqlite.Database } & BulkDeleteOperation<T>) => Promise<Array<Entity<T>>>;
+	createBulk?: (args: { db: sqlite.Database } & BulkCreateOperation<T>) => Promise<Array<T>>;
+	readBulk?: (args: { db: sqlite.Database } & BulkReadOperation<T>) => Promise<Array<T>>;
+	updateBulk?: (args: { db: sqlite.Database } & BulkUpdateOperation<T>) => Promise<Array<T>>;
+	deleteBulk?: (args: { db: sqlite.Database } & BulkDeleteOperation<T>) => Promise<Array<T>>;
 }
